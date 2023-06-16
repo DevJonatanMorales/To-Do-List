6a246b2692f54535a330fda9aef298e10753a717
DevJonatanMorales <developerjonatanmorales@gmail.com>
2023-06-16 17:40:50 -0600

    paginacion

---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,64 @@
+import React, { useContext } from "react";
+import { DataContext } from "../../context/ContextLocalStorage";;
+import "./pagination.css"
+
+export const Pagination = () => {
+  const { tareas, setPosition, position } = useContext(DataContext)
+
+  const first = () => setPosition(0);
+  const increment = () => {
+    if (position === tareas.length - 1) {
+      setPosition(position)
+    } else {
+      setPosition(position + 1)
+    }
+  };
+  const decrement = () => {
+    if (position === 0) {
+      setPosition(position)
+    } else {
+      setPosition(tareas != null ? position - 1 : position)
+    }
+  }
+
+  const last = () => setPosition(tareas.length - 1);
+
+  return (
+    <fieldset className="paginacion">
+      <legend className="paginacion__title">Navegación</legend>
+      <div className="paginacion__btns">
+        <button
+          className="paginacion__btn"
+          type="button"
+          onClick={() => { first() }}
+        >
+          <i className="fa-solid fa-angles-left"></i>
+        </button>
+        <button
+          className="paginacion__btn"
+          type="button"
+          onClick={() => { decrement() }}
+        >
+          <i className="fa-solid fa-angle-left"></i>
+        </button>
+
+        <p className="paginacion__info"> {position + 1} / {tareas != null ? tareas.length : '0'} </p>
+
+        <button
+          className="paginacion__btn"
+          type="button"
+          onClick={() => { increment() }}
+        >
+          <i className="fa-solid fa-chevron-right"></i>
+        </button>
+        <button
+          className="paginacion__btn"
+          type="button"
+          onClick={() => { last() }}
+        >
+          <i className="fa-solid fa-angles-right"></i>
+        </button>
+      </div>
+    </fieldset>
+  );
+};
